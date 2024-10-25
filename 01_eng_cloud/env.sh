256469dff37f9b561d3248d6695e59086a9ddaea
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-10-25 17:06:19 -0400

    Added a script to get .env for examples

---
+++ 01_eng_cloud/env.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+set -e
+
+S3_URL=s3://jojjiw/satellite_ma/llamaindex/01_eng_cloud/.env
+
+function get {
+    echo Retrieve .env from S3
+    aws s3 cp $S3_URL .env
+}
+
+function save {
+    echo Saving .env to S3
+    aws s3 cp .env $S3_URL
+}
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 {get|save}"
+    exit 1
+fi
+
+case $1 in
+    get)
+        get
+        ;;
+    save)
+        save
+        ;;
+    *)
+        echo "Invalid argument. Usage: $0 {get|save}"
+        exit 1
+        ;;
+esac
